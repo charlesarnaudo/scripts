21c9edc19ce62876635248602ae8e189b73a54d0
Charles Arnaudo <charlesarnaudo@gmail.com>
2017-07-03 14:48:07 -0400

    Added setaws sh script

---
+++ setaws.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo ""
+
+accounts=("" "")
+
+echo "Select from the following the accounts"
+
+index=1
+for i in ${accounts[@]}
+do
+  echo  "  $index" $i
+  index=`expr $index + 1`
+done
+
+read input
+
+if [[ $input -gt ${#accounts[@]} || $input -lt 1 ]]; then
+  echo "Not a valid account number"
+  exit 1
+
+elif [[ $input -eq 1 ]]; then
+  export AWS_ACCESS_KEY_ID="";export AWS_SECRET_ACCESS_KEY="";export AWS_REGION="us-east-1" ;
+elif [[ $input -eq 2 ]]; then
+  export AWS_ACCESS_KEY_ID="";export AWS_SECRET_ACCESS_KEY="";export AWS_REGION="us-east-1" ;
+fi
+
+echo ""
