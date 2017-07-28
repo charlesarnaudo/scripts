6fac1ee6e4f8a65e6fca0e2639febf1c92e29a25
Charles Arnaudo <charlesarnaudo@gmail.com>
2017-07-28 16:21:09 -0400

    Updated makehome

--- makehome.sh
+++ makehome.sh
@@ -1,4 +1,15 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew install brew-cask
+
+#apps
+brew cask install google-chrome
 brew cask install spotify
 brew cask install sublime-text
+
+#development
+brew install terraform
+brew install ansible
+brew install python3
+pip install virtualenv
+brew cask install java
