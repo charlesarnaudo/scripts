0242fd48e4b978b504c07d876eeff304db2ddc26
Charles Arnaudo <charlesarnaudo@gmail.com>
2019-05-17 15:48:38 -0400

    Create count_requests.py

---
+++ count_requests.py
@@ -0,0 +1,18 @@
+# Count the number of requests a unique host made given a logfile
+def parse_hostname(line):
+    return (line.split(" ")[0])
+
+filename = input()
+request_counts = {}
+
+with open(filename) as fp:
+    for line in fp:
+        hostname = parse_hostname(line)
+        if hostname not in request_counts:
+            request_counts[hostname] = 1
+        else:
+            request_counts[hostname] += 1
+
+with open("records_%s" % filename, 'w') as fp:
+    for k, v in sorted(request_counts.items()):
+        fp.write("%s %s\n" % (k, v))
